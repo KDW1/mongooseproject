ae48a50604b956772cd7f76857def5eae2d8200e
King-Diorr Willsun <51097023+KDW1@users.noreply.github.com>
2023-06-22 20:58:33 -0500

    Created posts router

--- app.js
+++ app.js
@@ -7,20 +7,14 @@ const uri = `mongodb+srv://diorrw:${process.env.password}@mongoose-tut.pvextla.m
 const PORT = 3000;
 const client = new MongoClient(uri);
 
-//Connect to DB
+const postsRoute = require('./routes/posts')
 
-app.use('/posts', () => {
-    console.log('This is a middleware running')
-})
+app.use('/posts', postsRoute)
 
 app.get('/', (req, res) => {
     res.send("Whats up dude")
 })
 
-
-app.get('/posts', (req, res) => {
-    res.send("We're on posts")
-})
 
 app.listen((PORT), () => {
     console.log("Listening on port: " + PORT);
@@ -28,12 +22,12 @@ app.listen((PORT), () => {
 
 async function run() {
     try {
-      const database = client.db('sample_mflix');
-      const movies = database.collection('movies');
-      // Query for a movie that has the title 'Back to the Future'
-      const query = { title: 'Back to the Future' };
-      const movie = await movies.findOne(query);
-      console.log(movie);
+    //   const database = client.db('sample_mflix');
+    //   const movies = database.collection('movies');
+    //   // Query for a movie that has the title 'Back to the Future'
+    //   const query = { title: 'Back to the Future' };
+    //   const movie = await movies.findOne(query);
+    //   console.log(movie);
     } finally {
       // Ensures that the client will close when you finish/error
       await client.close();
